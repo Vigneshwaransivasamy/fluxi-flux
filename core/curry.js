b611e3b6134deb91423c8730198089eedc327b09
vigneshwaransivasamy <vigneshwaransivasamy@gmail.com>
2017-10-06 20:25:56 +0530

    proxr added progress

--- core/curry.js
+++ core/curry.js
@@ -1,25 +1,8 @@
-var filler = require('./filler');
-console.log(filler);
-
-add2 = function (a,b){
-    return a+b;
-}
-
-curryN(2, add2);
-
-function curry2(targetFunction){
-    switch(arguments.length){
-        case 0:
-            return targetFunction;
-        case 1:
-            
-    }
-}
-
-module.exports = function curryN(argumentCount, targetFunction){
-    switch(arguments.length){
-        case 0:
-            return targetFunction;
-        case 1:
-    }
+module.exports = function curryN(fn){
+	return function(){
+      if(fn.length == arguments.length)  
+        return fn(...arguments);
+	  else    
+	    return fn.bind(null,...arguments);   
+	}
 }
